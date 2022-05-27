2e33746284433a76bdf1d3050b1250ad13b744e5
ernes <ernestogpradas@gmail.com>
2022-05-27 10:31:57 +0200

    T06 - Se implementa la funcionalidad de calcular la ráiz cuadrada en la calculadora sin utilizar la librería math o numpy

--- CalculadoraCientifica.py
+++ CalculadoraCientifica.py
@@ -13,3 +13,13 @@ def resta(a, b):
             raise TypeError
 
     return a - b
+
+
+# La raíz cuadrada no es más que elevar cualquier número a 0.5.
+# Usaremos la función pow() de python que devuelve el valor de un número elevado a una potencia especificada.
+# Este método también lo podríamos realizar con "**" que funciona igual que pow().
+def raiz_cuadrada(a):
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError
+
+    return pow(a, 0.5)
