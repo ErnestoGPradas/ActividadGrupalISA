1306b9d48b6f6ad75ebd5220d771842f818a3ac0
Marta B de la Rocha <mbarbeitodelarocha@gmail.com>
2022-05-29 19:51:06 +0200

    Pruebas unitarias: multiplicación, división y exponencial

--- pruebas_Unitarias.py
+++ pruebas_Unitarias.py
@@ -19,11 +19,29 @@ class TestUnitarios(unittest.TestCase):
     def test_resta2(self):
         self.assertEqual(CalculadoraCientifica.resta(5.8, 12), -6.2)
 
+    def test_multiplicacion1(self):
+        self.assertEqual(CalculadoraCientifica.multiplicacion(3, 7), 21)
+
+    def test_multiplicacion2(self):
+        self.assertEqual(CalculadoraCientifica.multiplicacion(7, 2.5), 17.5)
+
+    def test_division1(self):
+        self.assertEqual(CalculadoraCientifica.division(35, 7), 5)
+
+    def test_division2(self):
+        self.assertEqual(CalculadoraCientifica.division(14, 4), 3.5)
+
     def test_raiz_cuadrada1(self):
         self.assertEqual(CalculadoraCientifica.raiz_cuadrada(9), 3)
 
     def test_raiz_cuadrada2(self):
         self.assertEqual(CalculadoraCientifica.raiz_cuadrada(2), 1.4142135623730951)
+        
+    def test_exponencial1(self):
+        self.assertEqual(CalculadoraCientifica.exponencial(0), 1)
+
+    def test_exponencial2(self):
+        self.assertEqual(CalculadoraCientifica.exponencial(2), 7.389056099)
 
 
 if __name__ == "__main__":
