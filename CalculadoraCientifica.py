dbb6e54da93d0eb36ba85dbfd62a7c3685bda36b
ernes <ernestogpradas@gmail.com>
2022-06-01 19:35:06 +0200

    T09 - Se sube la implementación del método de aproximación babilónico para la raíz cuadrada

--- CalculadoraCientifica.py
+++ CalculadoraCientifica.py
@@ -1,3 +1,6 @@
+import colorama
+from colorama import Fore
+
 # Comprobamos que los números introducidos son enteros o flotantes
 def suma(a, b):
     for n in (a, b):
@@ -38,3 +41,70 @@ def raiz_cuadrada(a):
         raise TypeError
 
     return pow(a, 0.5)
+
+# También podemos realizar la raíz cuadrada con el método de aproximación babilónico
+def raiz_cuadradaBabilonico(a):
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError
+
+    x = a / 2
+
+    for i in range(20):
+        if x * x == a:
+            return x
+        else:
+            x = (x + (a/x)) / 2
+    return x
+
+
+
+# Comienza la calculadora
+
+arrancarCalculadora = True
+
+while arrancarCalculadora:
+    print(Fore.LIGHTCYAN_EX + """
+        Bienvenido a la calculadora. Selecciona la operación que deseas realizar:
+        
+        1) Sumar
+        2) Restar
+        3) Multiplicar 
+        4) Dividir
+        5) Raíz cuadrada método pow()
+        6) Raíz cuadrada método aproximáción babilónico
+        7) Apagar calculadora
+        """)
+    opcion = int(input("Elige una opción: ") )
+
+    if(opcion == 1 or opcion == 2 or opcion == 3 or opcion == 4):
+        n1 = float(input("Introduce tu primer número: ") )
+        n2 = float(input("Introduce tu segundo número: ") )
+    elif (opcion == 5 or opcion == 6):
+        n3 = float(input("Introduce un número: ") )
+
+
+    if opcion == 1:
+        print(" ")
+        print(Fore.GREEN + "RESULTADO: La suma de",n1,"+",n2,"es igual a ", suma(n1, n2))
+    elif opcion == 2:
+        print(" ")
+        print(Fore.GREEN + "RESULTADO: La resta de",n1,"-",n2,"es igual a ",resta(n1, n2))
+    elif opcion == 3:
+        print(" ")
+        print(Fore.GREEN + "RESULTADO: El producto de",n1,"*",n2,"es igual a ",multiplicacion(n1, n2))
+    elif opcion == 4:
+        print(" ")
+        print(Fore.GREEN + "RESULTADO: La división de",n1,"*",n2,"es igual a ",division(n1, n2))
+    elif opcion == 5:
+        print(" ")
+        print(Fore.GREEN + "RESULTADO: La raíz cuadrada (método pow()) de",n3,"es ",raiz_cuadrada(n3))
+    elif opcion == 6:
+        print(" ")
+        print(Fore.GREEN + "RESULTADO: La raíz cuadrada (aproximación babilónico) de",n3,"es ",raiz_cuadradaBabilonico(n3))
+    elif opcion == 7:
+        print(" ")
+        print(Fore.RED + "Apagando calculadora....")
+        arrancarCalculadora = False
+    else:
+        print("Opción incorrecta")
